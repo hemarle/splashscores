85a0abf4defb6d680a4d5fd4da77f9536fc110da
Daramola Femi <dfmopeoluwa@gmail.com>
2022-01-12 22:42:39 +0000

    match standing done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Detail from './Detail';
 import Home from './Home'
 import Navigation from './Navigation';
 import Splash from './Splash';
+import Standing from './Standing';
 function App() {
   return (
     <div className="app">
       {/* <Home/> */}
       {/* <Splash/> */}
+      {/* <Detail/> */}
+<Standing/>
+
+
+
+
       <Navigation/>
-      <Detail/>
      
     </div>
   );
