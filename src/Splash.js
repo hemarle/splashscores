c9b3c84c35dc9cbd610d3e6397bb9d7ee89b379d
Daramola Femi <dfmopeoluwa@gmail.com>
2022-01-12 19:27:29 +0000

    Splash mobile done

--- src/Splash.js
+++ src/Splash.js
@@ -1,19 +1,37 @@
 import React from "react";
 import "./Splash.css";
+import barcaIcon from "./images/barcelona.png";
+import realmadridIcon from "./images/realmadrid.png";
+import flagIcon from "./images/flag.svg";
+import footballIcon from "./images/football.svg";
+import basketballIcon from "./images/basketball.svg";
+import soccerIcon from "./images/soccer.svg";
+import newsIcon from "./images/news.svg";
+import arrowIcon from "./images/arrow.svg";
 function Splash() {
   return (
     <div className="splash">
+      <div className="splash__News">
+        <div className="splash__NewsLeft">
+          <h3>Liverpool UEFA Champions League Celebration</h3>
+          <p> Yesterday, 06:30pm</p>
+        </div>
+        <div className="splash__NewsRight">
+          <img src={newsIcon} alt="" />
+        </div>
+      </div>
+
       <div className="splash__Options">
         <div className="splash__Option">
-          <img src="" alt="" />
+          <img src={soccerIcon} alt="" />
           <p>Soccer</p>
         </div>
         <div className="splash__Option">
-          <img src="" alt="" />
+          <img src={basketballIcon} alt="" />
           <p>Basketball</p>
         </div>
         <div className="splash__Option">
-          <img src="" alt="" />
+          <img src={footballIcon} alt="" />
           <p>Football</p>
         </div>
       </div>
@@ -21,27 +39,61 @@ function Splash() {
       <div className="splash__Content">
         <div className="splash__Card">
           <div className="splash__CardTitle">
-            <img src="" alt="" />
-            <div className="league">
-              <h5>La Liga</h5>
-              <p>Spain</p>
+            <div className="splash__CardTitleLeft">
+              <img src={flagIcon} alt="" />
+              <div className="league">
+                <h5 className="league__Name">La Liga</h5>
+                <p className="league__Country">Spain</p>
+              </div>
             </div>
-            <p>></p>
+            <img src={arrowIcon} alt="" />
           </div>
 
           <div className="splash__CardDetails">
             <div className="splash__CardDetail">
-              <img src="" alt="" />
-              <img src="" alt="" />
+              <div className="teams-icons">
+                <img src={barcaIcon} alt="" />
+                <img src={realmadridIcon} alt="" />
+              </div>
               <div className="splash__Teams">
                 <h5 className="team">Barcelona vs Real Madrid</h5>
                 <p className="score">
                   2 <span>-</span> <span>0</span>
                 </p>
               </div>
-              <div className="">
-                  <h4>HT</h4>
-                  </div>
+              <div className="scoreRight">
+                <h4>HT</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="splash__Card">
+          <div className="splash__CardTitle">
+            <div className="splash__CardTitleLeft">
+              <img src={flagIcon} alt="" />
+              <div className="league">
+                <h5 className="league__Name">La Liga</h5>
+                <p className="league__Country">Spain</p>
+              </div>
+            </div>
+            <img src={arrowIcon} alt="" />
+          </div>
+
+          <div className="splash__CardDetails">
+            <div className="splash__CardDetail">
+              <div className="teams-icons">
+                <img src={barcaIcon} alt="" />
+                <img src={realmadridIcon} alt="" />
+              </div>
+              <div className="splash__Teams">
+                <h5 className="team">Barcelona vs Real Madrid</h5>
+                <p className="score">
+                  2 <span>-</span> <span>0</span>
+                </p>
+              </div>
+              <div className="scoreRight">
+                <h4>HT</h4>
+              </div>
             </div>
           </div>
         </div>
