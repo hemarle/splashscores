b7d9c42cdd64a7d69ba249d1c1897bdc15b1eb9a
Daramola Femi <dfmopeoluwa@gmail.com>
2022-01-12 19:36:44 +0000

    Navigation html done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from './Home'
+import Navigation from './Navigation';
 import Splash from './Splash';
 function App() {
   return (
     <div className="app">
       {/* <Home/> */}
       <Splash/>
+      <Navigation/>
      
     </div>
   );
