8f6ab80f536ebc18be770d27303b06b1bfc10729
Daramola Femi <dfmopeoluwa@gmail.com>
2022-01-14 15:18:41 +0000

    livescores api testing

--- src/Backend.js
+++ src/Backend.js
@@ -2,10 +2,29 @@ import React, { useState, useEffect } from "react";
 
 function Backend() {
   const [apiData, setApiData] = useState([]);
-  const getApiData = fetch("url")
-    .then((data) => data.json)
-    .then((data) => setApiData(data))
-    .catch((err) => console.log(err));
+
+  useEffect(() => {
+    const getApiData = fetch(
+      "https://livescore-api.com/api-client/scores/live.json&key=FZrLOaiRCSeaDtnR&secret=TJv9jRSQPbMScbtSbUDZ4WRHO5Rsyfjs",
+      {
+        method: "GET",
+        headers: {
+          "Accept":
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+          "Access-Control-Allow-Origin": "*",
+          "User-Agent":
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0",
+        },
+      }
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        setApiData(data);
+        console.log(data);
+      })
+
+      .catch((err) => console.log(err));
+  }, []);
 
   return <div></div>;
 }
